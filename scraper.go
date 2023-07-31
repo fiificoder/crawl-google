48514fc2ec7fa334e9ba400bf514430b78a91ced
Elvis Koranteng <119888850+fiificoder@users.noreply.github.com>
2023-07-31 18:15:22 +0000

    Update scraper.go

--- scraper.go
+++ scraper.go
@@ -9,38 +9,31 @@ import (
 )
 
 func main() {
-	// Set the search query
 	query := url.Values{"q": {"golang web scraper"}}
 
-	// Make an HTTP GET request to the Google search URL
 	res, err := http.Get("https://www.google.com/search?" + query.Encode())
 	if err != nil {
 		panic(err)
 	}
 	defer res.Body.Close()
 
-	// Parse the response as HTML
 	doc, err := html.Parse(res.Body)
 	if err != nil {
 		panic(err)
 	}
 
-	// Use the `Visit` function to iterate through all the nodes in the HTML document
 	var f func(*html.Node)
 	f = func(n *html.Node) {
-		// Check if the node is an anchor tag
+
 		if n.Type == html.ElementNode && n.Data == "a" {
-			// Iterate through the anchor tag's attributes
 			for _, attr := range n.Attr {
-				// Check if the attribute is the "href" attribute
 				if attr.Key == "href" {
-					// Print the value of the "href" attribute
 					fmt.Println(attr.Val)
 					break
 				}
 			}
 		}
-		// Recursively visit all the child nodes
+
 		for c := n.FirstChild; c != nil; c = c.NextSibling {
 			f(c)
 		}
